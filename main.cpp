5142e8809c158595bc6663bc452a3eca9b09dac6
Hieracon <37873846+Hieracon@users.noreply.github.com>
2018-11-22 00:15:54 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 int main()
 {
     List obj;
-    obj.add_element();
-    obj.print_list()
+    obj.add_element(struct node *head ,struct node *tail ,3);
+    obj.print_list(struct node *head, struct node *tail)
 }
